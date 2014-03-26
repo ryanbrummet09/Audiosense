d0d540bfd750b64044b9da1310f6c1c93a7ca9e7
syedshabihhasan <hasanshabih@gmail.com>
2014-03-26 14:52:07 -0500

    Changed the framing to account for bit masks

--- AudioCode/AudioPipeline/framing.m
+++ AudioCode/AudioPipeline/framing.m
@@ -1,4 +1,4 @@
-function [ frames ] = framing( audioSignal, frequency, frameSizeSeconds )
+function [ buzzMask, beepMask, frames ] = framing( audioSignal, frequency, frameSizeSeconds, locs_buzz, locs_beep )
 %FRAMING frames the given file in frames of size given as input
 %   FRAMING is the first component of the audio pipeline. It takes as input
 %   three things:
@@ -20,6 +20,20 @@ function [ frames ] = framing( audioSignal, frequency, frameSizeSeconds )
 %audioSignal = getSoundData(filename);
 frameSizeSamples = frequency .* frameSizeSeconds;
 frames = enframe(audioSignal,frameSizeSamples,frameSizeSamples,'r');
+buzzMask = false(1,length(frames));
+beepMask = false(1,length(frames));
 
+for P=1:length(locs_buzz)
+    frameNo = ceil(locs_buzz(P)./frameSizeSamples);
+    if false == buzzMask(frameNo)
+        buzzMask(frameNo) = true;
+    end
 end
 
+for P=1:length(locs_beep)
+    frameNo = ceil(locs_beep(P)./frameSizeSamples);
+    if false == beepMask(frameNo)
+        beepMask(frameNo) = true;
+    end
+end
+end
