27462eda3e37163dfb93642191c4f5978800b451
syedshabihhasan <hasanshabih@gmail.com>
2014-04-03 13:01:18 -0500

    Refactored some code.

---
+++ AudioCode/AudioPipeline/TestingAndValidation/RMS/runME.m
@@ -0,0 +1,10 @@
+function [ leFrames, cumLEFramesFiles ] = runME( frameSizeInSeconds, frequency,pctlV );
+%RUNME A working example of how to run the RMS evaluation
+%   Details are provided in rmsThresholdCalculation and in VerifyLowEnergy
+%   See also, RMSTHRESHOLDCALCULATION, VERIFYLOWENERGY
+addpath ../;
+addpath ../voicebox;
+[rmsValues, totalRMS, pctl,pname, fname] = rmsThresholdCalculation(frameSizeInSeconds,frequency);
+[ leFrames,cumLEFramesFiles ] = VerifyLowEnergy( pname,fname,pctl, frameSizeInSeconds, frequency, pctlV );
+end
+
