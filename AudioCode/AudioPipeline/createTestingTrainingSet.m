9a5a4560d9a70061a4a552aafac71ae1a1e54928
syedshabihhasan <hasanshabih@gmail.com>
2014-04-12 16:44:50 -0500

    Added the GMM training code, modified the testing and training set creation.

--- AudioCode/AudioPipeline/createTestingTrainingSet.m
+++ AudioCode/AudioPipeline/createTestingTrainingSet.m
@@ -1,4 +1,4 @@
-function [ trainingSet, testingSet ] = createTestingTrainingSet( trueFeatureMatrix )
+function [ trainingSet, testingSet, GMMSet ] = createTestingTrainingSet( trueFeatureMatrix )
 %CREATETESTINGTRAININGSET creates the training and test sets of features
 %   This function randomly subsamples the input data. The size of the
 %   training set is 90% of the input feature matrix. The testing set is the
@@ -10,21 +10,25 @@ function [ trainingSet, testingSet ] = createTestingTrainingSet( trueFeatureMatr
 %
 %   Output:
 %           trainingSet             :       Training set, created by random
-%                                           subsampling (with replacement).
+%                                           subsampling (w/o replacement).
 %                                           The size is 90% the size of the
 %                                           feature matrix
 %           testingSet              :       Testing set, containing the        
 %                                           remaining elements (the ones
 %                                           not selected by the training
 %                                           set.
+%           GMMSet                  :       Randomly subsampled training
+%                                           set (with replacement) for
+%                                           creating the GMM
 %   Usage:
 %   [trainingSet, testingSet] = createTestingTrainingSet(trueFeatureMatrix)
 %
 %   See also GETTRUEFEATURES, DATASAMPLE
-
-trainingSetSize = ceil(0.9 .* length(trueFeatureMatrix));
-[trainingSet, idx] = datasample(trueFeatureMatrix,trainingSetSize,true);
+[r c] = size(trueFeatureMatrix);
+trainingSetSize = ceil(0.9 .* r);
+[trainingSet, idx] = datasample(trueFeatureMatrix,trainingSetSize);
 testingSet = [];
+[GMMSet,gmmidx] = datasample(trainingSet,ceil(0.6*trainingSetSize),true);
 for P=1:length(trueFeatureMatrix)
     if isempty(find(idx==P))
         testingSet(end+1,:) = trueFeatureMatrix(P,:);
