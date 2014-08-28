70b8818181adfb888e3a9b4d5333e18221c54419
syedshabihhasan <hasanshabih@gmail.com>
2014-08-28 09:32:14 -0500

    main script to run feature extraction

---
+++ AudioCode/AudioPipeline/extractingAudioClips/runPipeline.m
@@ -0,0 +1,42 @@
+function runPipeline( fileList, fs, mfccCoff, srfLimits, ...
+    frameSizeInSeconds, numberOfSubbands )
+%RUNPIPELINE Summary of this function goes here
+%   Detailed explanation goes here
+
+%% add dependencies
+addpath ../;
+addpath ../voicebox/;
+
+%% create parallel workers
+parobject = parpool;
+%% read audio file
+n = length(fileList);
+subbands = getLogSubbands(fs, numberOfSubbands);
+parfor P=1:n
+    fname = fileList{P};
+    disp(fname);
+    data = getSoundData(fname);
+    [bz,bp,frames] = framing(data,fs,frameSizeInSeconds);
+    [r,c] = size(frames);
+    featureVector = [];
+    lastSpectralAmp = [];
+    for Q=1:r
+        if 1 == Q
+            [fv, lastSpectralAmp] = extractClipFrameFeatures( data, fs, ...
+               mfccCoff, srfLimits, subbands);
+        else
+            [fv, lastSpectralAmp] = extractClipFrameFeatures( data, fs, ...
+                mfccCoff, srfLimits, subbands, lastSpectralAmp);
+        end
+        featureVector(end+1,:) = fv;
+    end
+    toSaveFname = strsplit(fname,'/');
+    toSaveFname = toSaveFname{end};
+    toSaveFname = strsplit(toSaveFname,'.');
+    toSaveFname = strcat('features/',toSaveFname{1});
+    parSaveVariable(toSaveFname,featureVector);
+end
+%% close parallel workers
+delete(parobject);
+end
+
