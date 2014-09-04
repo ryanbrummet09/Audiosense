4a8676b2ac90cc1c860d7a6e2b7ff23903abd68d
syedshabihhasan <hasanshabih@gmail.com>
2014-09-04 16:25:55 -0500

    added the capability to read wav files

--- AudioCode/AudioPipeline/getSoundData.m
+++ AudioCode/AudioPipeline/getSoundData.m
@@ -1,10 +1,15 @@
-function [ data ] = getSoundData( fileName )
+function [ data ] = getSoundData( fileName, wavFiles )
 %GETSOUNDDATA Extracts the sound samples
 %   This takes as input the file name and outputs the samples of audio
 %   signal present in it. There are a few assumptions made in this
 %   function, first, the file is a raw binary file; second, the samples are
-%   stored as shorts. Third, the data is in the little endian format.
-f = fopen(fileName);
-data = fread(f,inf,'short',0,'l');
-fclose(f);
+%   stored as shorts. Third, the data is in the little endian format. There
+%   is an optional parameter which indicates if the file is a wav file
+if 1 == nargin
+    f = fopen(fileName);
+    data = fread(f,inf,'short',0,'l');
+    fclose(f);
+else
+    [data,fs] = audioread(filename);
+end
 end
