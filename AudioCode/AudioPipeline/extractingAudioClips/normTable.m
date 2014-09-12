22e28a5eee523ac7a65c7c13ed0ae4f68c8251fc
syedshabihhasan <hasanshabih@gmail.com>
2014-09-12 17:58:49 -0500

    made this a little more robust

--- AudioCode/AudioPipeline/extractingAudioClips/normTable.m
+++ AudioCode/AudioPipeline/extractingAudioClips/normTable.m
@@ -1,15 +1,18 @@
 function [ featureTable ] = normTable( featureTable )
 %NORMTABLE normalize all the features
-%   take the z transform individually on each column
+%
 
 varNames = featureTable.Properties.VariableNames;
 n = length(varNames);
 for P=5:n-1
     varN = varNames{P};
     val = eval(sprintf('featureTable.%s;',varN));
-    val_mean = mean(val);
-    val_std = std(val);
-    val = (val - val_mean)/val_std;
+    ninf = find(~isinf(val) & ~isnan(val));
+    iinf = find(isinf(val) | isnan(val));
+    val_mean = mean(val(ninf));
+    val_std = std(val(ninf));
+    val(ninf) = (val(ninf) - val_mean)/val_std;
+    val(iinf) = 0;
     eval(sprintf('featureTable.%s = val;',varN));
 end
 
