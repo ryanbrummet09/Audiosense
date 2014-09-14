6962f057a0a3e341f287a58ea6f0ce3687f15ee5
syedshabihhasan <hasanshabih@gmail.com>
2014-09-14 17:19:49 -0500

    create the probability matrix

---
+++ AudioCode/AudioPipeline/GaussianFunctions/getProbabilityMatrix.m
@@ -0,0 +1,32 @@
+function [ prMatrix ] = getProbabilityMatrix( dataSet, k )
+%GETPROBABILITYMATRIX creates the probability matrix
+%   Input:
+%           dataSet         :           Dataset containing features, this 
+%                                       is a matrix
+%           k               :           the number of gaussians
+% 
+%   Output:
+%           prMatrix        :           matrix containing the information
+%                                       of all patients
+
+pids = dataSet(:,1);
+cids = dataSet(:,2);
+sids = dataSet(:,3);
+
+temp = {};
+for P=1:length(pids)
+    temp{end+1} = strcat(num2str(pids(P)),'_',num2str(cids(P)),'_',...
+                         num2str(sids(P)));
+end
+temp = unique(temp);
+n = length(temp);
+prMatrix = zeros(n,k+3);
+for P=1:n
+    t = temp{P};
+    t = strsplit(t,'_');
+    prMatrix(P,1) = str2num(t{1});
+    prMatrix(P,2) = str2num(t{2});
+    prMatrix(P,3) = str2num(t{3});
+end
+end
+
