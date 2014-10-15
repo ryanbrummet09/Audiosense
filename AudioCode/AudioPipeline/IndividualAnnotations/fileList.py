566df976c9e55ed033b505f73617ea0c76dad395
syedshabihhasan <hasanshabih@gmail.com>
2014-10-15 14:41:38 -0500

    delete existing files

--- AudioCode/AudioPipeline/IndividualAnnotations/fileList.py
+++
@@ -1,9 +0,0 @@
-import fnmatch;
-import os;
-
-def fileList(toLookIn, fileType):
-	fileList = [];
-	for root,dirs, files in os.walk(toLookIn):
-		for filename in fnmatch.filter(files,'*.'+fileType):
-			 fileList.append(os.path.join(root,filename));
-	return fileList;
