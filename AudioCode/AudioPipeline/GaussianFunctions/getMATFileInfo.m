e6b6463a406fc178be0ad8bb58313c38d6276d12
syedshabihhasan <hasanshabih@gmail.com>
2014-10-19 17:18:38 -0500

    added the ability to deal with multiple labels

--- AudioCode/AudioPipeline/GaussianFunctions/getMATFileInfo.m
+++ AudioCode/AudioPipeline/GaussianFunctions/getMATFileInfo.m
@@ -1,14 +1,29 @@
-function [ pid, cid, sid ] = getMATFileInfo( fileName )
+function [ pid, cid, sid, otherInfo ] = getMATFileInfo( fileName,multLabel )
 %GETMATFILEINFO get identification information from mat file
 %   the file has to be of the format:
 %   anynumberoffolders/pid_cid_sid_anything.mat
+%   the multLabel flag indicates that we also want the label info
 
+if 1 == nargin
+    multLabel = false;
+end
 fileName = strsplit(fileName,'/');
 fileName = fileName{end};
 fileName = strsplit(fileName,'_');
 pid = str2num(fileName{1});
 cid = str2num(fileName{2});
 sid = str2num(fileName{3});
+otherInfo = {};
+if multLabel
+    otherInfo = fileName{4};
+    if strcmpi(fileName{4},'media')
+        otherInfo = strcat(fileName{4},'_',fileName{5});
+    end
+    % the next line is just some mop up work because of a spelling error
+    if strcmpi(otherInfo,'speecj')
+        otherInfo = 'speech';
+    end
+end
 
 end
 
