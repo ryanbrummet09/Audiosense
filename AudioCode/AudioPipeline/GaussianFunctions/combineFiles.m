b70063b6d5c3cff5e94ce6c45325f43afc38e2ae
syedshabihhasan <hasanshabih@gmail.com>
2014-10-22 16:59:00 -0500

    combining of files can now delete the label containing columns

--- AudioCode/AudioPipeline/GaussianFunctions/combineFiles.m
+++ AudioCode/AudioPipeline/GaussianFunctions/combineFiles.m
@@ -1,4 +1,5 @@
-function [ combinedSet ] = combineFiles( fileList, labelOrder )
+function [ combinedSet ] = combineFiles( fileList, labelOrder, ...
+                            notIncludeVectorLabels, numberToNotInclude )
 %COMBINEFILES combine the file features into matrices
 %   The file list is a cell array that contains the file paths to the
 %   feature files, these are loaded and combined into a single matrix
@@ -8,10 +9,19 @@ function [ combinedSet ] = combineFiles( fileList, labelOrder )
 %   essentially populates the 4th column to the combinedSet output to
 %   indicates the position of the label in the cell array labelOrder.
 %   Eg. labelOrder = {'speech','media_speech','media_music','music'}
+%   If the notIncludeVectorLabels flag is set to true, the labels
+%   associated with the extracted features are not included in the combined
+%   file. This is also an optional flag. The number of labels to not
+%   include from the end is defined in numberToNotInclude.
 combinedSet = [];
 multLabels = false;
-if 2 == nargin
+if 2 <= nargin & ~isempty(labelOrder)
     multLabels = true;
+    if 3 == nargin
+        numberToNotInclude = 0;
+    end
+elseif 1 == nargin
+    notIncludeVectorLabels = false;
 end
 for P=1:length(fileList)
         disp(sprintf('Working with %d, %s',P,fileList{P}));
@@ -32,7 +42,11 @@ for P=1:length(fileList)
         temp(:,2) = cid;
         temp(:,3) = sid;
         temp(:,4) = idx;
-        temp(:,5:end) = fileFeatures;
+        if notIncludeVectorLabels
+            temp(:,5:end) = fileFeatures(:,1:end-numberToNotInclude);
+        else
+            temp(:,5,end) = fileFeatures
+        end
         if 1 == P
             combinedSet = temp;
         else
