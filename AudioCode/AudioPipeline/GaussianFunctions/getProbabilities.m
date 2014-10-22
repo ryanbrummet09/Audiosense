38edea2a4d988a05834f576e3da99779ef7fbcec
syedshabihhasan <hasanshabih@gmail.com>
2014-10-22 17:01:03 -0500

    the probability calculation can now populate prMatrix with values for each frame of each file rather than just aggregating them. It also includes the labels, if wanted.

--- AudioCode/AudioPipeline/GaussianFunctions/getProbabilities.m
+++ AudioCode/AudioPipeline/GaussianFunctions/getProbabilities.m
@@ -1,6 +1,7 @@
 function [ prMatrix ] = getProbabilities( GMMObject, dataSet, ...
                                             k, prMatrix,...
-                                            softCoding)
+                                            includesLabelVector,...
+                                            numberOfLabels,softCoding)
 %GETPROBABILITIES get the probability distribution of each point
 %   Input:
 %           GMMObject           :           The GMM object obtained from
@@ -12,6 +13,10 @@ function [ prMatrix ] = getProbabilities( GMMObject, dataSet, ...
 %           prMatrix            :           The matrix that needs to be
 %                                           filled with the probabilities,
 %                                           each row represents one file
+%           includesLabelVector :           flag to indicate that the label
+%                                           vectors are present in the
+%                                           dataSet
+%           numberOfLabels      :           the number of distinct labels
 %           softCoding          :           this is an optional flag which
 %                                           should be set to true when one
 %                                           wants to work with the actual
@@ -22,33 +27,58 @@ function [ prMatrix ] = getProbabilities( GMMObject, dataSet, ...
 
 if 4 == nargin
     softCoding = false;
+    includesLabelVector = false;
+    numberOfLabels = 0;
+elseif 5==nargin
+%     since the number of labels has not been specified, we do not want to
+%     take them into account
+    softCoding = false;
+    includesLabelVector = false;
+    numberOfLabels = 0;
+elseif 6 == nargin
+    softCoding = false;
 end
-dataSet = normValues(dataSet);
+dataSet = normValues(dataSet,numberOfLabels);
 allMu = GMMObject.mu;
 allSigma = GMMObject.Sigma;
 [r,c] = size(dataSet);
-gaussProbs = posterior(GMMObject,dataSet(:,5:end));
-for P=1:r
-    if softCoding
+gaussProbs = posterior(GMMObject,dataSet(:,5:end-numberOfLabels));
+if ~includesLabelVector
+    for P=1:r
+        if softCoding
+                tempV = prMatrix(prMatrix(:,1)==dataSet(P,1) & ...
+                        prMatrix(:,2)==dataSet(P,2) & ...
+                        prMatrix(:,3)==dataSet(P,3) & ...
+                        prMatrix(:,4)==dataSet(P,4),5:end);
+                prMatrix(prMatrix(:,1)==dataSet(P,1) & ...
+                    prMatrix(:,2)==dataSet(P,2) & ...
+                    prMatrix(:,3)==dataSet(P,3) & ...
+                    prMatrix(:,4)==dataSet(P,4),5:end) = tempV + ...
+                                                         gaussProbs(P,:);
+        else
+            idx = find(gaussProbs(P,:) == max(gaussProbs(P,:)));
             tempV = prMatrix(prMatrix(:,1)==dataSet(P,1) & ...
                     prMatrix(:,2)==dataSet(P,2) & ...
                     prMatrix(:,3)==dataSet(P,3) & ...
-                    prMatrix(:,4)==dataSet(P,4),5:end);
+                    prMatrix(:,4)==dataSet(P,4),4+idx);
             prMatrix(prMatrix(:,1)==dataSet(P,1) & ...
-                prMatrix(:,2)==dataSet(P,2) & ...
-                prMatrix(:,3)==dataSet(P,3) & ...
-                prMatrix(:,4)==dataSet(P,4),5:end) = tempV + ...
-                                                     gaussProbs(P,:);
-    else
-        idx = find(gaussProbs(P,:) == max(gaussProbs(P,:)));
-        tempV = prMatrix(prMatrix(:,1)==dataSet(P,1) & ...
-                prMatrix(:,2)==dataSet(P,2) & ...
-                prMatrix(:,3)==dataSet(P,3) & ...
-                prMatrix(:,4)==dataSet(P,4),4+idx);
-        prMatrix(prMatrix(:,1)==dataSet(P,1) & ...
-                prMatrix(:,2)==dataSet(P,2) & ...
-                prMatrix(:,3)==dataSet(P,3) & ...
-                prMatrix(:,4)==dataSet(P,4),4+idx) = tempV+1;
+                    prMatrix(:,2)==dataSet(P,2) & ...
+                    prMatrix(:,3)==dataSet(P,3) & ...
+                    prMatrix(:,4)==dataSet(P,4),4+idx) = tempV+1;
+        end
+    end
+else
+    for P=1:r
+        if softCoding
+            prMatrix(P,1:4) = dataSet(P,1:4);
+            prMatrix(P,5:4+k) = gaussProbs(P,:);
+            prMatrix(P,5+k:end) = dataSet(P,end-numberOfLabels+1:end);
+        else
+            prMatrix(P,1:4) = dataSet(P,1:4);
+            idx = find(gaussProbs(P,:) == max(gaussProbs(P,:)));
+            prMatrix(P,4+idx) = 1;
+            prMatrix(P,5+k:end) = dataSet(P,end-numberOfLabels+1:end);
+        end
     end
 end
 
