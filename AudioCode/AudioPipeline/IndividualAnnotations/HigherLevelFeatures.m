697b680b0d6dfd75521645dc55374e3532cb80e6
syedshabihhasan <hasanshabih@gmail.com>
2014-11-17 17:58:14 -0600

    modified to handle the label vectors along with single labels

--- AudioCode/AudioPipeline/IndividualAnnotations/HigherLevelFeatures.m
+++ AudioCode/AudioPipeline/IndividualAnnotations/HigherLevelFeatures.m
@@ -1,4 +1,4 @@
-function [ featureTable ] = HigherLevelFeatures( dataStruct )
+function [ featureTable ] = HigherLevelFeatures( dataStruct, labelVector )
 %HIGHERLEVELFEATURES extract higher level features
 %   Input:
 %           dataStruct      :       data structure containing the details
@@ -8,22 +8,78 @@ function [ featureTable ] = HigherLevelFeatures( dataStruct )
 %                                   represents the conditionID, sid
 %                                   represents the session ID, 
 %                                   features represent the matrix
-%                                   containing the features
+%                                   containing the features, in the case
+%                                   when labelVector is true, then the
+%                                   field label_name should contain the
+%                                   actual label names for each column in
+%                                   the label vector. There is an optional
+%                                   flag , extras, that is used when
+%                                   variable number of MFCCs, SRFs, and
+%                                   subband powers are calculated.
+%           labelVector     :       an optional flag indicating if the
+%                                   label data structure is a vector
 % 
 %   Output:
 %           featureTable    :       the table contains the higher level
 %                                   features
+if 1 == nargin
+    labelVector = false;
+end
 featureList = {'minV','maxV',...
     'stdV','median','1qr','3qr','iqr','skewnessV','kurtosisV',...
     'noOfPeaks','meanAmpPeaks'};
-dataFeatures = {'rms', 'zcr', 'mfcc0', 'mfcc1', 'mfcc2', 'mfcc3', ...
-    'mfcc4', 'mfcc5', 'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', ...
-    'mfcc11', 'mfcc12','srf25','srf50','srf75','srf90','sflux',...
-    'scentroid','sentropy','sbp0','sbp1','sbp2','sbp3','sbp4',...
-    'sbp5','sbp6','sbp7'}; 
+if isfield(dataStruct,'extras')
+    if dataStruct.extras
+        noOfMFCC = dataStruct.mfcc;
+        SRFs = dataStruct.srf;
+        noOfSBP = dataStruct.sbp;
+        dataFeatures = {'rms','zcr'};
+        if noOfMFCC > 0
+            for P=0:noOfMFCC
+                dataFeatures{end+1} = sprintf('mfcc%d',P);
+            end
+        end
+        if length(SRFs) > 0
+            for P=1:length(SRFs)
+                dataFeatures{end+1} = sprintf('srf%.2f',SRFs(P));
+            end
+        end
+        dataFeatures{end+1} = {'sflux'};
+        dataFeatures{end+1} = {'scentroid'};
+        dataFeatures{end+1} = {'sentropy'};
+        if noOfSBP > 0
+            for P=0:noOfSBP-1
+                dataFeatures{end+1} = sprintf('sbp%d',P);
+            end
+        end
+    else
+        dataFeatures = {'rms', 'zcr', ....
+            'mfcc0', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5',...
+            'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11',...
+            'mfcc12',....
+            'srf25','srf50','srf75','srf90',...
+            'sflux','scentroid','sentropy',....
+            'sbp0','sbp1','sbp2','sbp3','sbp4','sbp5','sbp6','sbp7'}; 
+    end
+else
+    dataFeatures = {'rms', 'zcr', ....
+        'mfcc0', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5',...
+        'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11',...
+        'mfcc12',....
+        'srf25','srf50','srf75','srf90',...
+        'sflux','scentroid','sentropy',....
+        'sbp0','sbp1','sbp2','sbp3','sbp4','sbp5','sbp6','sbp7'}; 
+end
 data = dataStruct.features;
 featureVector = {};
 featureVariableNames = {'patient','condition','session'};
+featureVector{1,end+1} = dataStruct.pid;
+featureVector{1,end+1} = dataStruct.cid;
+featureVector{1,end+1} = dataStruct.sid;
+if labelVector
+    featureVector{1,end+1} = dataStruct.date;
+    featureVariableNames{1,end+1} = 'date';
+end
 for P=1:length(dataFeatures)
     featureN = dataFeatures{P};
     for Q=1:length(featureList)
@@ -31,9 +87,6 @@ for P=1:length(dataFeatures)
         featureVariableNames{1,end+1} = temp;
     end
 end
-featureVector{1,end+1} = dataStruct.pid;
-featureVector{1,end+1} = dataStruct.cid;
-featureVector{1,end+1} = dataStruct.sid;
 [r,c] = size(data);
 % per feature extract the high level features
 for P=1:c
@@ -59,14 +112,31 @@ for P=1:c
 %     kurtosis
     featureVector{1,end+1} = kurtosis(featureColumn);
 %     number of peaks
-    [peakAmps, peakLocs] = findpeaks(abs(featureColumn));
+    [peakAmps, ~] = findpeaks(abs(featureColumn));
     featureVector{1,end+1} = length(peakAmps);
 %     mean amplitude of peaks
     featureVector{1,end+1} = mean(peakAmps);
 end
-featureVector{1,end+1} = dataStruct.label;
 featureTable = table;
+if ~labelVector
+    featureVector{1,end+1} = dataStruct.label;
+    featureVariableNames{1,end+1} = 'label';
+else
+    for P=1:length(dataStruct.label_name)
+        featureVariableNames{1,end+1} = dataStruct.label_name{P};
+    end
+    [r_l,c_l] = size(dataStruct.label);
+    labelV = zeros(1,c_l);
+    labelV = labelV - 1;
+    for P=1:r_l
+        rowV = dataStruct.label(P,:);
+        labelV(find(rowV == 1)) = 1;
+    end
+    for P=1:c_l
+        featureVector{1,end+1} = labelV(P);
+    end
+    %featureVector{1,end+1:end+c_l} = labelV;
+end
 featureTable = cell2table(featureVector);
-featureVariableNames{1,end+1} = 'label';
 featureTable.Properties.VariableNames = featureVariableNames;
 end
