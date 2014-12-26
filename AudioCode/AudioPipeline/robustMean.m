0516336ed69bb036b447506c0eef5a1e9ebc1e09
syedshabihhasan <hasanshabih@gmail.com>
2014-12-25 18:55:46 -0600

    a mean robust to nan's and inf's

---
+++ AudioCode/AudioPipeline/robustMean.m
@@ -0,0 +1,17 @@
+function [ opArray ] = robustMean( ipArray )
+%ROBUSTMEAN mean function that removes nan's and inf's
+
+[r,c] = size(ipArray);
+opArray = zeros(1,c);
+for P=1:c
+    temp = ipArray(:,P);
+    toKeep = true(r,1);
+    nanidx = find(isnan(temp));
+    infidx = find(isinf(temp));
+    toKeep(nanidx) = false;
+    toKeep(infidx) = false;
+    opArray(1,P) = mean(temp);
+end
+
+end
+
