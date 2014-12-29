aa90886b83cde458ef64bda57603b72117ff5eae
syedshabihhasan <hasanshabih@gmail.com>
2014-12-28 20:14:59 -0600

    generic robust stat function

---
+++ AudioCode/AudioPipeline/robustStat.m
@@ -0,0 +1,21 @@
+function [ opArray ] = robustStat( ipArray, statFuncHandle )
+%ROBUSTSTAT statistic calculations robust of nans and infs
+
+[r,c] = size(ipArray);
+opArray = zeros(1,c);
+for P=1:c
+    temp = ipArray(:,P);
+    toKeep = true(r,1);
+    nanidx = find(isnan(temp));
+    infidx = find(isinf(temp));
+    toKeep(nanidx) = false;
+    toKeep(infidx) = false;
+    opArray(1,P) = feval(statFuncHandle, temp(toKeep));
+end
+
+end
+
+
+
+end
+
