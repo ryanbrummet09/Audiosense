95900b6dd21cc249d7be703f935e701deb161b19
syedshabihhasan <hasanshabih@gmail.com>
2014-12-31 17:56:55 -0600

    user can specify the min max values

--- AudioCode/AudioPipeline/scaleValues.m
+++ AudioCode/AudioPipeline/scaleValues.m
@@ -1,7 +1,12 @@
-function [ scaledMatrix ] = scaleValues( ipMatrix, leftV, rightV )
+function [ scaledMatrix, minMaxV ] = scaleValues( ipMatrix, leftV, ...
+                                        rightV, minMaxV )
 %SCALEVALUES scales values of ipMatrix between leftV and rightV
 %   Detailed explanation goes here
-
+minMax = true;
+if 3 == nargin
+    minMaxV = [];
+    minMax = false;
+end
 [r,c] = size(ipMatrix);
 scaledMatrix = zeros(r,c);
 for P=1:c
@@ -10,8 +15,15 @@ for P=1:c
     toKeep(idx) = false;
     idx = find(isinf(ipMatrix(:,P)));
     toKeep(idx) = false;
-    minV = min(ipMatrix(toKeep,P));
-    maxV = max(ipMatrix(toKeep,P));
+    if ~minMax
+        minV = min(ipMatrix(toKeep,P));
+        maxV = max(ipMatrix(toKeep,P));
+        minMaxV(P,1) = minV;
+        minMaxV(P,2) = maxV;
+    else
+        minV = minMaxV(P,1);
+        maxV = minMaxV(P,2);
+    end
     scaledMatrix(toKeep,P) = ...
     (((ipMatrix(toKeep,P)-minV)/(maxV-minV))*(rightV-leftV))+leftV;
     scaledMatrix(~toKeep,P) = 0;
