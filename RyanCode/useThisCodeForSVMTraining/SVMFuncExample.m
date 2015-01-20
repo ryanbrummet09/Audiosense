d74e58628a5b51c43f55ec7e2f244ccb4b28bb47
ryanbrummet09 <ryan-brummet@uiowa.edu>
2015-01-20 16:55:50 -0600

    updated example

--- RyanCode/useThisCodeForSVMTraining/SVMFuncExample.m
+++ RyanCode/useThisCodeForSVMTraining/SVMFuncExample.m
@@ -1,20 +1,21 @@
 close all;
 clear;
 clc;
-
 inputStruct = struct;
 inputStruct.libSVMLibLocation = '/Users/ryanbrummet/Documents/MATLAB/Extensions';
 inputStruct.dirsToPath = {'/Users/ryanbrummet/Documents/MATLAB/Audiology/ExtractedResponseSets/'};
-inputStruct.saveLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/SurveyStuff/Results/tempRBF';
+inputStruct.saveLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/SurveyStuff/Results/temp';
 inputStruct.dataLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/ExtractedResponseSets/spSurvey.mat';
 inputStruct.response = {'sp'};
 inputStruct.numCores = 2;
-inputStruct.toRemove = {'ac','lc','tf','tl','vc','cp','rs','nz','nl'};
+%inputStruct.kernal = 0;
+%inputStruct.toRemove = {'ac','lc','tf','tl','vc','cp','rs','nz','nl'};
 inputStruct.makePlot = true;
 inputStruct.groupVars = {'patient','condition'};
 inputStruct.maxIterCount = 1;
-inputStruct.startGammaValues = [1];
-inputStruct.startCostValues = [1,10,100,1000,10000,50000];
+inputStruct.startGammaValues = [.01];
+inputStruct.startCostValues = [1000];
 inputStruct.allowGridSearchEdgeVals = true;
 
-[ SVMSettings, mdlStruct, absErrorStruct ] = SVMFunc( inputStruct );
+[ SVMSettings, mdlStruct, errorStruct ] = SVMFunc( inputStruct );
+
