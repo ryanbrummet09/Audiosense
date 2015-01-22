9b1e8d39109cc9c66249fce5110e09b679bfc95b
ryanbrummet09 <ryan-brummet@uiowa.edu>
2015-01-21 20:44:41 -0600

    updated example for SVMFunc

--- RyanCode/useThisCodeForSVMTraining/SVMFuncExample.m
+++ RyanCode/useThisCodeForSVMTraining/SVMFuncExample.m
@@ -2,19 +2,20 @@ close all;
 clear;
 clc;
 inputStruct = struct;
-inputStruct.libSVMLibLocation = '/Users/ryanbrummet/Documents/MATLAB/Extensions';
+inputStruct.libLocation = '/Users/ryanbrummet/Documents/MATLAB/Extensions';
+inputStruct.libToUse = 1;
 inputStruct.dirsToPath = {'/Users/ryanbrummet/Documents/MATLAB/Audiology/ExtractedResponseSets/'};
-inputStruct.saveLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/SurveyStuff/Results/temp';
-inputStruct.dataLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/ExtractedResponseSets/spSurvey.mat';
+inputStruct.saveLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/SurveyStuff/Results/stSurveyResults';
+inputStruct.dataLocation = '/Users/ryanbrummet/Documents/MATLAB/Audiology/ExtractedResponseSets/stSurvey.mat';
 inputStruct.response = {'sp'};
 inputStruct.numCores = 2;
-%inputStruct.kernal = 0;
+inputStruct.kernel = 2;    % use 11, 12, or 13 for libLinear  %use 0-3 for libSVM
 %inputStruct.toRemove = {'ac','lc','tf','tl','vc','cp','rs','nz','nl'};
 inputStruct.makePlot = true;
 inputStruct.groupVars = {'patient','condition'};
 inputStruct.maxIterCount = 1;
-inputStruct.startGammaValues = [.01];
-inputStruct.startCostValues = [1000];
+inputStruct.startGammaValues = [.001,.01,.1];
+inputStruct.startCostValues = [10000,100000,500000];
 inputStruct.allowGridSearchEdgeVals = true;
 
 [ SVMSettings, mdlStruct, errorStruct ] = SVMFunc( inputStruct );
