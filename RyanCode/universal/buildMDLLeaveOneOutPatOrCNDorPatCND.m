379e5cb5e3cbe046d169bafb89e1416c6d5bdb6a
ryanbrummet09 <ryan-brummet@uiowa.edu>
2015-01-23 12:39:09 -0600

    added new funcs and mod code

---
+++ RyanCode/universal/buildMDLLeaveOneOutPatOrCNDorPatCND.m
@@ -0,0 +1,7 @@
+function [ output_args ] = buildMDLLeaveOneOutPatOrCNDorPatCND( input_args )
+%BUILDMDLLEAVEONEOUTPATORCNDORPATCND Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ RyanCode/universal/buildMDLPerPatorCNDorPatCND.m
@@ -0,0 +1,7 @@
+function [ output_args ] = buildPatorCNDorPatCNDMDL( input_args )
+%BUILDPATORCNDORPATCNDMDL Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
