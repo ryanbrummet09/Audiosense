aedefc1ae1a5672b4fe059129a253f84270e49e8
ryanbrummet09 <brummet@uiowa.edu>
2015-01-29 14:56:10 -0600

    Delete buildMDLLeaveOneOutPatOrCNDorPatCND.m

--- RyanCode/universal/buildMDLLeaveOneOutPatOrCNDorPatCND.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = buildMDLLeaveOneOutPatOrCNDorPatCND( input_args )
-%BUILDMDLLEAVEONEOUTPATORCNDORPATCND Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
